59c7d185b3b610b76c72598c07ad76b9b28e532f
ShadowDark93 <davidortegacadena@gmail.com>
2022-08-30 21:39:08 -0500

    upgrade angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PrincipalComponent } from './components/principal/principal.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { PortafolioComponent } from './components/portafolio/portafolio.component';
+import { WhyUsComponent } from './components/why-us/why-us.component';
+import { TiendasComponent } from './components/tiendas/tiendas.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PrincipalComponent,
+    InicioComponent,
+    NosotrosComponent,
+    PortafolioComponent,
+    WhyUsComponent,
+    TiendasComponent,
+    ContactoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]

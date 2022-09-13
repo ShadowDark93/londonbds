2ba8a82142b1782648345644cee3818cb3b031e7
ShadowDark93 <davidortegacadena@gmail.com>
2022-09-12 23:48:38 -0500

    traducciones de font basico

--- front/src/app/components/navbar/navbar.component.ts
+++ front/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public translate: TranslateService) {
+    translate.addLangs(['en', 'es']);
+    translate.setDefaultLang('en');
+    translate.use('en');
+  }
 
   ngOnInit(): void {
+  }
+
+  switchLang = (lang: string) => {
+    this.translate.use(lang)
   }
 
 }
